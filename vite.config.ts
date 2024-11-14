2979ff6ef2facccddd00285062cf12f4126c8cd4
seoyoonyi <yiseoyoon@gmail.com>
2024-11-14 23:16:46 +0900

    다시 한번 더시도 프리뷰 나왔잖아 좋았잖아

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,29 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react({
-      jsxImportSource: '@emotion/react',
-      babel: {
-        plugins: ['@emotion/babel-plugin'],
-      },
-    }),
-    tsconfigPaths(),
-  ],
-  server: {
-    proxy: {
-      '/api': {
-        target: process.env.VITE_BASE_URL,
-        changeOrigin: true,
-        secure: false,
+
+export default defineConfig(({ mode }) => {
+  // .env 파일에서 환경 변수를 불러오기
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [
+      react({
+        jsxImportSource: '@emotion/react',
+        babel: {
+          plugins: ['@emotion/babel-plugin'],
+        },
+      }),
+      tsconfigPaths(),
+    ],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_BASE_URL,
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
+  };
 });
