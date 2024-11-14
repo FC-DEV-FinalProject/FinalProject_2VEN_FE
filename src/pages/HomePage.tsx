c733857a6c0d5d7cb4b8d386aaafe9c660e1d2a1
Hyejin <hyejin.frontend@gmail.com>
2024-11-15 00:08:49 +0900

    test: api 테스트 코드

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,29 @@
+import { useEffect } from 'react';
+
 import SignUpSuccessPage from '@/pages/auth/signup/SignUpSuccessPage';
 
-const HomePage = () => (
-  <div>
+const HomePage = () => {
+  useEffect(() => {
+    const apiHost = import.meta.env.VITE_BASE_URL;
+    const fetchNotices = async () => {
+      try {
+        console.log('Requesting URL:', `${apiHost}/api/notices`);
+        const response = await fetch(`${apiHost}/api/notices`);
+        console.log('Response status:', response.status);
+        const data = await response.json();
+        console.log('Response data:', data);
+      } catch (error) {
+        console.error('Error details:', error);
+      }
+    };
+    fetchNotices();
+  }, []);
+  return (
     <div>
-      <SignUpSuccessPage />
+      <div>
+        <SignUpSuccessPage />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 export default HomePage;
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,29 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react({
-      jsxImportSource: '@emotion/react',
-      babel: {
-        plugins: ['@emotion/babel-plugin'],
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [
+      react({
+        jsxImportSource: '@emotion/react',
+        babel: {
+          plugins: ['@emotion/babel-plugin'],
+        },
+      }),
+      tsconfigPaths(),
+    ],
+    server: {
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: env.VITE_BASE_URL,
+          changeOrigin: true,
+          secure: false,
+        },
       },
-    }),
-    tsconfigPaths(),
-  ],
-  server: {
-    port: 3000, // 어떤 포트로 서버를 열지
-  },
+    },
+  };
 });
