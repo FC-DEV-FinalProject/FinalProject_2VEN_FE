22b3079c06c5f15b46331a0ac3023a1ca98a87d1
Hyejin <hyejin.frontend@gmail.com>
2024-11-15 01:44:38 +0900

    fix: deploy error fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig(({ command, mode }) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
     plugins: [react(), tsconfigPaths()],
