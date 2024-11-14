5a49108b65205b72fec9f44232a66af0c1f1534e
Hyejin <hyejin.frontend@gmail.com>
2024-11-15 01:33:13 +0900

    test: 테스트 코드

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,12 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import SignUpSuccessPage from '@/pages/auth/signup/SignUpSuccessPage';
 
 const HomePage = () => {
+  const [noticesData, setNoticesData] = useState([]);
+  useEffect(() => {
+    console.log('Updated noticesData:', noticesData);
+  }, [noticesData]);
   useEffect(() => {
     console.log('VITE_BASE_URL:', import.meta.env.VITE_BASE_URL);
     const fetchNotices = async () => {
@@ -12,6 +16,7 @@ const HomePage = () => {
         console.log('Response status:', response.status);
         const data = await response.json();
         console.log('Response data:', data);
+        setNoticesData(data);
       } catch (error) {
         console.error('Error details:', error);
       }
