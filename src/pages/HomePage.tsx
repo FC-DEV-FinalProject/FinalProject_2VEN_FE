0d9a98f7d3e6d555fe1d1288229ace2c8468730c
Hyejin <hyejin.frontend@gmail.com>
2024-11-15 00:31:49 +0900

    test: api 테스트

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,11 +4,11 @@ import SignUpSuccessPage from '@/pages/auth/signup/SignUpSuccessPage';
 
 const HomePage = () => {
   useEffect(() => {
-    const apiHost = import.meta.env.VITE_BASE_URL;
+    console.log('VITE_BASE_URL:', import.meta.env.VITE_BASE_URL);
     const fetchNotices = async () => {
       try {
-        console.log('Requesting URL:', `${apiHost}/api/notices`);
-        const response = await fetch(`${apiHost}/api/notices`);
+        console.log('Requesting URL:', `${import.meta.env.VITE_BASE_URL}/api/notices`);
+        const response = await fetch('/api/notices');
         console.log('Response status:', response.status);
         const data = await response.json();
         console.log('Response data:', data);
